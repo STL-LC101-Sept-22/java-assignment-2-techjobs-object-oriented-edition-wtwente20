f44618d07732b99982221285fd40e361fe885c80
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-02-09 19:57:09 -0600

    Now would be a good time to save, commit, and push your work up to GitHub.

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -16,7 +16,12 @@ public class Job {
     // TODO: Add two constructors - one to initialize a unique ID and a second to initialize the
     //  other five fields. The second constructor should also call the first in order to initialize
     //  the 'id' field.
+    public Job() {
+        id = nextId;
+        nextId++;
+    }
 
+    public Job(String name, )
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
     //  match.
 
